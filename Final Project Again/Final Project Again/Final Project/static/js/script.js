7ac3a0399e36b2ed449c5bd4a57ac95574020b51
juanansaldo <jansaldo@csumb.edu>
2023-07-24 20:27:41 -0700

    deleted unused 'Final Project Again' folder and 'main.py'

--- Final Project Again/Final Project Again/Final Project/static/js/script.js
+++
@@ -1,8 +0,0 @@
-function submitForm(event) {
-    event.preventDefault(); // Prevent the form from submitting normally
-
-    const searchQuery = document.getElementById('searchInput').value.toLowerCase();
-    const url = '/detail/' + searchQuery
-
-    window.location.href = url;
-}
--- Final Project Again/Final Project Again/main.py
+++
@@ -1,4 +0,0 @@
-# Team 7558
-# CST 205
-
-print('hello world')
